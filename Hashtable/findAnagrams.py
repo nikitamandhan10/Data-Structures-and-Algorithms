e9331ba26ca166675b3277a3953aedd8bcaf1daa
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-02-02 22:57:54 -0800

    Update findAnagrams.py

--- Hashtable/findAnagrams.py
+++ Hashtable/findAnagrams.py
@@ -1,3 +1,9 @@
+'''
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+Example 1:
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+'''
 def findAnagrams(self, s: str, p: str) -> List[int]:
   if len(s) < len(p):
       return []
