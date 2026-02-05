b87f6f3fbf43674c7b6f654bed78b23d098ecb85
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-02-04 21:40:59 -0800

    Update subarraySum.py

--- Hashtable/subarraySum.py
+++ Hashtable/subarraySum.py
@@ -1,3 +1,8 @@
+'''
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+'''
 def subarraySum(self, nums: List[int], k: int) -> int:
   prefix = {0 : 1}
   curr = res = 0
