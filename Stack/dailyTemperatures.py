2acc6fce4fe3263f5548f518ca7aa69da431d9f3
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-02-18 19:29:17 -0800

    Update dailyTemperatures.py

--- Stack/dailyTemperatures.py
+++ Stack/dailyTemperatures.py
@@ -8,3 +8,14 @@ Output: [1,1,4,2,1,1,0,0]
 Example 2:
 
 '''
+
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+  stack = []
+  res = [0] * len(temperatures)
+  for i, t in enumerate(temperatures):
+      while stack and stack[-1][0] < t:
+          res[stack[-1][1]] = i - stack[-1][1]
+          stack.pop()
+
+      stack.append([t, i])
+  return res
