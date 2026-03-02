d02713774bf5e1203a6552632c4b8a6608c004ba
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-03-01 19:29:36 -0800

    Update minMeetingRooms.py

--- Heap/minMeetingRooms.py
+++ Heap/minMeetingRooms.py
@@ -6,3 +6,14 @@ Example 1:
 Input: intervals = [[0,30],[5,10],[15,20]]
 Output: 2
 '''
+
+class Solution:
+  def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+      intervals.sort()
+      endTime = [intervals[0][1]]
+      for interval in intervals[1:]:
+          if interval[0] >= endTime[0]:
+              heappop(endTime)
+          heappush(endTime, interval[1])
+      
+      return len(endTime)
