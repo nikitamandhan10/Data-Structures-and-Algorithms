3274938849f821c3d7dbc6ae0a70a690b059a450
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-03-03 14:49:43 -0800

    Update ProductOfNumbers.py

--- Data Stream/ProductOfNumbers.py
+++ Data Stream/ProductOfNumbers.py
@@ -16,3 +16,30 @@ Input
 Output
 [null,null,null,null,null,null,20,40,0,null,32]
 '''
+
+
+class ProductOfNumbers:
+  def __init__(self):
+      self.prefix_product = [1]
+  
+  def add(self, num):
+      if num == 0:
+          self.prefix_product = [1]
+      else:
+          self.prefix_product.append(self.prefix_product[-1] * num)
+  
+  def getProduct(self, k):
+      if len(self.prefix_product) <= k:
+          return 0
+      else:
+          return self.prefix_product[-1] // self.prefix_product[-k-1]
+
+
+
+
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
