f53a4be1dd69a86cc06a9533f8cc3c66c0604754
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-03-16 23:45:26 -0700

    Create nextGreaterElement.py

---
+++ Stack/nextGreaterElement.py
@@ -0,0 +1,28 @@
+'''
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. 
+If there is no next greater element, then the answer for this query is -1.
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+Example 1:
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+'''
+
+class Solution:
+  def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+      nIdx = {}
+      for i, n in enumerate(nums1):
+          nIdx[n] = i
+
+      res = [-1] * len(nums1)
+
+      stack = []
+      for n in nums2:
+          while stack and stack[-1] < n:
+              res[nIdx[stack[-1]]] = n
+              stack.pop()
+          if n in nIdx:
+              stack.append(n)
+      return res
